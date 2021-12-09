3938ecebda6106057f4b82ccb8465ea21d3612b7
Tambosa <tambosa@gmail.com>
2021-12-09 09:35:56 +0300

    Homework1 -data Class -textView -onClick

--- app/src/main/java/com/aroman/kotlinproject1/MainActivity.kt
+++ app/src/main/java/com/aroman/kotlinproject1/MainActivity.kt
@@ -2,10 +2,25 @@ package com.aroman.kotlinproject1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.clickButton)
+        textView = findViewById(R.id.text_view_number)
+        data class MyClass(var Title: String, var number: Int)
+
+        val my = MyClass("Test", 1)
+
+        button.setOnClickListener {
+            textView.text = my.number++.toString()
+        }
     }
 }
